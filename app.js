499920ffa44feb6f58d1aeaf035d1322b6f4856b
Alec Keeler <alec.keeler@gmail.com>
2022-06-13 17:46:01 -0500

    Tasks 3-4

--- app.js
+++ app.js
@@ -2,10 +2,43 @@
 const express = require('express');
 const app = express();
 
+// Task 4a
+app.use(express.json())
+
 // Task 2a
 app.get('/home', (req, res) => {
     // Task 2b
     res.send('Welcome Home')
+})
+
+let boardgames = [ //name, max_players, category, avg_rating
+    {name: 'Gloomhaven'},
+    {name: 'Pandemic Legacy: Season 1'},
+    {name: 'Brass: Birmingham'},
+    {name: 'Terraforming Mars'},
+    {name: 'Twilight Imperium: Fourth Edition'},
+    {name: 'Spirit Island'},
+    {name: 'Mage Knight'},
+    {name: 'Rising Sun'}
+]
+
+let reviews = [];
+
+// Task 3
+app.get(['/boardgames', '/games'], (req, res) => {
+    let names = [];
+    for (let i = 0; i < boardgames.length; i++) {
+        // const game = boardgames[i];
+        names.push(boardgames[i].name)
+    }
+    res.json({names})
+})
+
+// Task 4
+app.post('/reviews', (req, res) => {
+    console.log(req.body)
+    reviews.push(req.body)
+    res.json({message: "Success", reviews})
 })
 
 
